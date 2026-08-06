ae3872bf05957cff8f527d91038747756f14b0ed
maxiaotian520 <46263822+maxiaotian520@users.noreply.github.com>
2026-08-06 10:50:56 -0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,21 @@
+# # Brute Force Time O^2, Space O 1  双loop
+# class Solution:
+#     def maxArea(self, heights: List[int]) -> int:
+#         res = 0
+#         for i in range(len(heights)):
+#             for j in range(i + 1, len(heights)):
+#                 res = max(res, min(heights[i], heights[j]) * (j - i))
+#         return res
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights)-1
+        res = 0
+        while l < r:
+            area = min(heights[l], heights[r]) * (r-l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
